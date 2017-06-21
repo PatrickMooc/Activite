4624aca913ed31facb1eaed65218d7e920b36af2
Patrick <patrick.mooc@gmail.com>
2017-06-21 22:04:51 +0200

    ajout de commentaires dans Test_arguments.c

--- Test_arguments.c
+++ Test_arguments.c
@@ -3,6 +3,8 @@
 int main (int argc, char* *argv)
 {
     //printf("argc = %d\n", argc);
+    // argc = 0 est le nom de l'exécutable
+    // argc = 1 à argc est le nombre de paramètres passés
     if (argc > 1) {
         for (int i = 1; i < argc; i++){
             if (i == argc - 1) {
