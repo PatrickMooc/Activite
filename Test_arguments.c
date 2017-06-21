e7a93f234343609b582899c0479c43d05f439c4c
Patrick <patrick.mooc@gmail.com>
2017-06-21 22:10:26 +0200

    utilisation d'un tableau pour argv dans Test_arguments.c et mise à jour du fichier Test_arguments.txt

--- Test_arguments.c
+++ Test_arguments.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-int main (int argc, char* *argv)
+int main (int argc, char* argv[])
 {
     //printf("argc = %d\n", argc);
     // argc = 0 est le nom de l'exécutable
     // argc = 1 à argc est le nombre de paramètres passés
     if (argc > 1) {
         for (int i = 1; i < argc; i++){
+            //printf("argv[%d] = %s\n", i, argv[i]);
+            //printf("%s ", argv[i]);
             if (i == argc - 1) {
-                printf("%s", *argv);
+                printf("%s", argv[i]);
             } else {
-                printf("%s ", *argv++);
+                printf("%s ", argv[i]);
             }
         }
     }
